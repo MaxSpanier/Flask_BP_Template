71ed0659cc07f4a0db4930d443340d69e088f6d1
MaxSpanier <40826608+MaxSpanier@users.noreply.github.com>
2022-09-18 18:08:16 +0200

    Added a config file and the implementation

--- setup.py
+++ setup.py
@@ -9,12 +9,7 @@ setup(
     ],
 )
 
-# Rename the application
-# - Folders 
-# - packages in setup 
-# - name in views 
-# - name in __init__
-# - name in -flaskenv
 # Add requirements to the install_requires
 # Then "pip install -e .
 # flask --app flaskr --debug run
+# access to config variables: app.config["name"]
