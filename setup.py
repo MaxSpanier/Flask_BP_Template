07562504eb8e7ce72a23e03badfba63ba1b26c66
MaxSpanier <40826608+MaxSpanier@users.noreply.github.com>
2023-08-14 15:06:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ setup(
 # Add requirements to the install_requires
 # Then "pip install -e .
 # flask --app app --debug run --port=5005
+# ngrok http --domain=polecat-true-secretly.ngrok-free.app 5005
 # access to config variables: current_app.config["name"]
 
 # set the environment variables with: (Windows is set instead of export)
