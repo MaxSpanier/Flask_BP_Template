9593d6f0a70b10ad982181e27e47b83b6eed64e5
MaxSpanier <40826608+MaxSpanier@users.noreply.github.com>
2023-08-14 15:05:23 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
 
 # Add requirements to the install_requires
 # Then "pip install -e .
-# flask --app flaskr --debug run --port=5005
+# flask --app app --debug run --port=5005
 # access to config variables: current_app.config["name"]
 
 # set the environment variables with: (Windows is set instead of export)
